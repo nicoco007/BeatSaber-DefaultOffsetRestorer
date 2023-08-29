07b32ce24bc9d9fccfdbba5783c2920f7865e525
Nicolas Gnyra <nicolas@gnyra.com>
2023-08-28 23:33:38 -0400

    Apply offsets the same way as before 1.29.4

--- DefaultOffsetRestorer/Patches/UnityXRHelper.cs
+++ DefaultOffsetRestorer/Patches/UnityXRHelper.cs
@@ -20,7 +20,7 @@ namespace DefaultOffsetRestorer.Patches
 {
     /// <summary>
     /// Sets <see cref="UnityXRHelper._htcVivePoseOffset"/>, <see cref="UnityXRHelper._oculusPoseOffset"/>, and <see cref="UnityXRHelper._valveIndexPoseOffset"/>
-    /// so the offsets result in the same position as in Beat Saber before the OpenXR update.
+    /// so the offsets result in the "raw" SteamVR position rather than the "grip" position.
     /// </summary>
     [HarmonyPatch(typeof(UnityXRHelper), nameof(UnityXRHelper.Start))]
     internal static class UnityXRHelper_Start
@@ -34,21 +34,17 @@ namespace DefaultOffsetRestorer.Patches
         // SteamVR\drivers\indexcontroller\resources\rendermodels\valve_controller_knu_1_0_right\valve_controller_knu_1_0_right.json (grip)
         private static readonly Pose kKnucklesGripOffset = SteamVRPose(new Vector3(0.0f, -0.015f, 0.13f), new Vector3(15.392f, 2.071f, -0.303f));
 
-        private static readonly Pose kOriginalHtcViveOffset = new(new Vector3(0f, -0.008f, 0f), Quaternion.Euler(-4.3f, 0f, 0f));
-        private static readonly Pose kOriginalOculusOffset = new(new Vector3(0f, 0f, 0.055f), Quaternion.Euler(-40f, 0f, 0f)); // TODO: this is from OculusVRHelper; might be wrong
-        private static readonly Pose kOriginalKnucklesOffset = new(new Vector3(0f, 0.022f, -0.01f), Quaternion.Euler(-16.3f, 0f, 0f));
-
         private static void Prefix(UnityXRHelper __instance)
         {
-            __instance._htcVivePoseOffset = AddOffsets(kHtcViveGripOffset, kOriginalHtcViveOffset);
-            __instance._oculusPoseOffset = AddOffsets(kOculusGripOffset, kOriginalOculusOffset);
-            __instance._valveIndexPoseOffset = AddOffsets(kKnucklesGripOffset, kOriginalKnucklesOffset);
+            __instance._htcVivePoseOffset = InvertOffset(kHtcViveGripOffset);
+            __instance._oculusPoseOffset = InvertOffset(kOculusGripOffset);
+            __instance._valveIndexPoseOffset = InvertOffset(kKnucklesGripOffset);
         }
 
-        private static EulerPose AddOffsets(Pose gripOffset, Pose originalOffset)
+        private static EulerPose InvertOffset(Pose gripOffset)
         {
-            Vector3 localPosition = (Quaternion.Inverse(gripOffset.rotation) * -gripOffset.position) + (Quaternion.Inverse(gripOffset.rotation) * originalOffset.rotation * originalOffset.position);
-            Quaternion localRotation = Quaternion.Inverse(gripOffset.rotation) * originalOffset.rotation;
+            Vector3 localPosition = Quaternion.Inverse(gripOffset.rotation) * -gripOffset.position;
+            Quaternion localRotation = Quaternion.Inverse(gripOffset.rotation);
             return new EulerPose(localPosition, localRotation.eulerAngles);
         }
 
---
+++ DefaultOffsetRestorer/Patches/VRController.cs
@@ -0,0 +1,75 @@
+﻿// <copyright file="VRController.cs" company="nicoco007">
+// This file is part of DefaultOffsetRestorer.
+//
+// DefaultOffsetRestorer is free software: you can redistribute it and/or modify it under the terms
+// of the GNU General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// DefaultOffsetRestorer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with DefaultOffsetRestorer.
+// If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace DefaultOffsetRestorer.Patches
+{
+    /// <summary>
+    /// Applies the user's controller offsets the same way as they used to be before Beat Saber 1.29.4.
+    /// </summary>
+    [HarmonyPatch(typeof(VRController), nameof(VRController.TryGetControllerOffset))]
+    internal static class VRController_TryGetControllerOffset
+    {
+        private static bool Prefix(VRController __instance, ref bool __result, out Pose poseOffset)
+        {
+            if (__instance._vrPlatformHelper is not UnityXRHelper unityXRHelper)
+            {
+                poseOffset = default;
+                return true;
+            }
+
+            __result = __instance._vrPlatformHelper.TryGetPoseOffsetForNode(__instance._node, out poseOffset);
+            UnityXRController controller = unityXRHelper.ControllerFromNode(__instance._node);
+
+            if (__instance._transformOffset != null)
+            {
+                poseOffset = AdjustControllerPose(controller, poseOffset, __instance._transformOffset.positionOffset, __instance._transformOffset.rotationOffset);
+            }
+            else
+            {
+                poseOffset = AdjustControllerPose(controller, poseOffset, Vector3.zero, Vector3.zero);
+            }
+
+            if (__instance._node == XRNode.LeftHand)
+            {
+                poseOffset = VRController.InvertControllerPose(poseOffset);
+            }
+
+            return false;
+        }
+
+        // based on OpenVRHelper's AdjustControllerTransform
+        private static Pose AdjustControllerPose(UnityXRController controller, Pose poseOffset, Vector3 position, Vector3 rotation)
+        {
+            if (controller.manufacturerName == UnityXRHelper.VRControllerManufacturerName.Valve)
+            {
+                rotation += new Vector3(-16.3f, 0f, 0f);
+                position += new Vector3(0f, 0.022f, -0.01f);
+            }
+            else
+            {
+                rotation += new Vector3(-4.3f, 0f, 0f);
+                position += new Vector3(0f, -0.008f, 0f);
+            }
+
+            // The original code does transform.Rotate(rotation) then transform.Translate(position)
+            // so we have to rotate the position by both the pose offset and the rotation.
+            return new Pose(poseOffset.position + (poseOffset.rotation * Quaternion.Euler(rotation) * position), poseOffset.rotation * Quaternion.Euler(rotation));
+        }
+    }
+}
